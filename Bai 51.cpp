2fa49be77272a705842844ae090bb78ebb5246a5
K_Haruto1103 <126055950+NTDHaruto@users.noreply.github.com>
2023-07-04 21:23:52 +0700

    4/7/2023

---
+++ Bai 51.cpp
@@ -0,0 +1,24 @@
+//Bai 51: Hay tim chu so lon nhat cua so nguyen duong n
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"Nhap so nguyen duong n: ";
+	cin>>n;
+	int temp1=n,max=0;
+	while(n!=0)
+	{
+		int temp2=n%10;
+		if(max<temp2)
+		{
+			max=temp2;
+		}
+		n/=10;
+	}
+	cout<<"Chu so lon nhat cua "<<temp1<<" la = "<<max<<endl;
+	return 0;
+}
