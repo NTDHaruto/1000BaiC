8f61acd286975ef93a6baebaf94be77d0c58d691
K_Haruto1103 <126055950+NTDHaruto@users.noreply.github.com>
2023-07-12 14:40:36 +0700

    87 88 89 (12/7/2023)

---
+++ Bai 88.cpp
@@ -0,0 +1,17 @@
+//Bai 88: Hay su dung vong lap for de xuat tat ca ky tu tu A den z
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char start='A';
+	char end='Z';
+	for(char i = start;i<=end;i++)
+	{
+		cout<<i<<"    ";
+	}
+	cout<<endl;
+	return 0;
+}
