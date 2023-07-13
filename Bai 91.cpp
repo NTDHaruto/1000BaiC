74e40e5f67eb791ed565d132df434b4849233e65
NguyenTrongDung <126055950+NTDHaruto@users.noreply.github.com>
2023-07-13 10:58:03 +0700

    91 94 95 (13/7/2023)
    Bài 91: In tất cả các số nguyên dương lẻ nhỏ hơn 100
    Bài 92: Tìm ước số chung lớn nhất của 2 số nguyên dương (đã có)
    Bài 93: Viết chương trình kiểm tra 1 số có phải là số nguyên tố hay không (đã có)
    Bài 94: Viết chương trình in ra tất cả các số lẻ nhỏ hơn 100 trừ các số 5, 7, 93
    Bài 95: Viết chương trình nhập 3 số thực. Hãy thay tất cả các số âm bằng trị tuyệt đối của nó

---
+++ Bai 91.cpp
@@ -0,0 +1,14 @@
+//Bai 91: In tat ca cac so nguyen duong le nho hon 100
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	for(int i=1;i<100;i+=2)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+}
---
+++ Bai 94.cpp
@@ -0,0 +1,17 @@
+//Bai 93: Viet chuong trinh in ra cac so le nho hon 100 tru cac so 5, 7, 93
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	for(int i=1;i<100;i+=2)
+	{
+		if(i!=5 || i!=7 || i!= 93)
+		{
+			cout<<i<<endl;
+		}
+	}
+	return 0;
+}
